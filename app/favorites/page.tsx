5a55168eabbe7b8c01e163d2a5e48d3291915568
code-haseeb <abdullhaseebonline@gmail.com>
2024-02-25 15:58:55 +0500

    favorite clients

---
+++ app/favorites/page.tsx
@@ -0,0 +1,8 @@
+
+
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getFavoriteListings from "@/app/actions/getFavoriteListings";
+
