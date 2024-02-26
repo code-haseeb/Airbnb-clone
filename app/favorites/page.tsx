9b1c9ee8c969df8bf4fcc099487da7873d5c7ca4
code-haseeb <abdullhaseebonline@gmail.com>
2024-02-26 14:01:48 +0500

    favorite client

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -6,3 +6,5 @@ import ClientOnly from "@/app/components/ClientOnly";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getFavoriteListings from "@/app/actions/getFavoriteListings";
 
+import FavoritesClient from "./FavoritesClient";
+
