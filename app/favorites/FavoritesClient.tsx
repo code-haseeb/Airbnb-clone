dda03b9a70b024a85c94a0c9e85028aeac015010
code-haseeb <abdullhaseebonline@gmail.com>
2024-03-01 11:50:53 +0500

    favorite client

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,6 +1,9 @@
 
 import { SafeListing, SafeUser } from "@/app/types";
 
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+
 interface FavoritesClientProps {
   listings: SafeListing[],
   currentUser?: SafeUser | null,
