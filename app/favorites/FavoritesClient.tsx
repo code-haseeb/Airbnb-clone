6ea36e71846ec684de8f1705ecea4f975e7d4a46
code-haseeb <abdullhaseebonline@gmail.com>
2024-03-07 14:07:15 +0500

    favorite client

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -19,6 +19,27 @@ return (
         title="Favorites"
         subtitle="List of places you favorited!"
       />
+      <div         
+              className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
     </Container>
 }
 
